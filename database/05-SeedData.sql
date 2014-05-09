d403801c408255c6263ed4e8fd50980d0289fbac
Muhammad Siddique <siddique@dataeng.com>
2014-05-09 14:01:00 +0500

    Implement triggers and data validation

---
+++ database/05-SeedData.sql
@@ -0,0 +1,19 @@
+/*
+ * Customer Master Data ETL
+ * Sample Data
+ * SQL Server 2008/2012
+ */
+
+USE CustomerMDM;
+GO
+
+-- Insert sample records
+INSERT INTO dbo.MainRecords (RecordCode, RecordName, Description, CreatedBy)
+VALUES
+('REC001', 'Sample Record 1', 'First sample record for Customer-Master-Data-ETL', 'admin'),
+('REC002', 'Sample Record 2', 'Second sample record for Customer-Master-Data-ETL', 'admin'),
+('REC003', 'Sample Record 3', 'Third sample record for Customer-Master-Data-ETL', 'admin');
+
+PRINT 'Sample data inserted successfully';
+PRINT 'Total records: ' + CAST(@@ROWCOUNT AS VARCHAR(10));
+GO
